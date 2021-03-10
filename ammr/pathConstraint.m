e4ac320585c0f6f28ce04d9cf9912d428e70e167
RogueDiesel <43709684+RogueDiesel@users.noreply.github.com>
2021-03-10 18:47:59 -0500

    Delete ammr directory

--- ammr/pathConstraint.m
+++
@@ -1,6 +0,0 @@
-function [c,ceq] = pathConstraint(x,u)
-x2 = x(2,:);
-up = u(1,:); uq = u(2,:);
-c = [-x2.*up; x2.*uq];
-ceq = up.*uq;
-end
--- ammr/pathOjective.m
+++
@@ -1,6 +0,0 @@
-function obj = pathOjective(x)
-
-x3 = x(3,:);
-obj = -30* x3.^2;
-
-end
